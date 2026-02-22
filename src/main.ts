c41ff73b956cf68f3da2c319410694ad7376270f
Wawa27 <32011804+Wawa27@users.noreply.github.com>
2026-02-22 17:34:33 +0100

    feat: add logging via winston (#2)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { WinstonModule } from 'nest-winston';
+import { winstonFactory } from './logger/winston.config';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  const configService = app.get(ConfigService);
+
+  app.useLogger(WinstonModule.createLogger(winstonFactory(configService)));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
